dbc6d9a3371c2e8b7bb2e1cc00606965bca5cff4
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-09-29 17:57:10 +0200

    Initial commit and structuring an UI for login form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import img1 from "./assets/img1.png";
+import img2 from "./assets/img2.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="signup-container">
+      <div className="img-1">
+        <img src={img1} alt={"welcome"}></img>
+      </div>
+      <div className="img-2">
+        <img src={img2} alt={"tree"}></img>
+      </div>
+      <div className="form-container">
+        <p className="title">New Account?</p>
+        <form className="signup-form">
+          <div className="form-item fullname">
+            <input type="text" placeholder="Your Full Name" />
+            <i className="fas fa-user"></i>
+          </div>
+          <div className="form-item email">
+            <input type="text" placeholder="Your Email" />
+            <i className="fas fa-envelope"></i>
+          </div>
+          <div className="form-item password">
+            <input type="password" placeholder="Your Password" />
+            <i className="fas fa-key"></i>
+          </div>
+          <div className="form-item confirm-password">
+            <input type="password" placeholder="Confirm Your Password" />
+            <i className="fas fa-user-lock"></i>
+          </div>
+          <button type="submit">Sign up</button>
+        </form>
+      </div>
     </div>
   );
 }
